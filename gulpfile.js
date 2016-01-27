79c3aaf7ec552b5d74b16e81a6f6dd2fe1dd9eec
Alexander Noriega <lambdatoast@gmail.com>
2016-01-26 23:37:31 -0300

    Add angular-ui-router through bower.json. Add ui-router typescript definitions. Add an index.html. Create basic gulp tasks for building the js bundles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var tsc = require('gulp-typescript');
 var gulp = require('gulp');
+var vendorSources = require('main-bower-files');
+var concat = require('gulp-concat');
+
+gulp.task('build', ['tsc', 'vendor']);
+
+gulp.task('vendor', function () {
+  return gulp.src(vendorSources())
+    .pipe(concat('vendor.build.js'))
+    .pipe(gulp.dest('www/js'));
+});
 
 gulp.task('tsc', function () {
   var sources = [
